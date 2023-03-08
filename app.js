09a5c69c4aed69c43af8983edc465ab265e8a281
marcofs10 <121571271+marcofs10@users.noreply.github.com>
2023-03-08 10:57:27 -0500

    Added UI to rps game

--- app.js
+++ app.js
@@ -1,45 +1,61 @@
-const options = ["rock","paper","scissors"];
-const victory = {"rock":"scissors","paper":"rock","scissors":"paper"}
+const options = ["rock", "paper", "scissors"];
+const victory = { "rock": "scissors", "paper": "rock", "scissors": "paper" }
 let playerPoint = 0;
 let compPoint = 0;
+let run = true;
 
-function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random()*3);
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
     return options[randomNumber];
 }
 
-function playerRound(computerSelection){
-    const playerPrompt = prompt("Make a choice: ");
-    const playerSelection= playerPrompt.toLowerCase();
-    if (playerSelection==computerSelection){
-        console.log("Draw");
-    }
-    else{
-        for(let i=0;i<3;i++){
-            if(playerSelection==Object.keys(victory)[i]){
-                if(computerSelection==victory[Object.keys(victory)[i]]){
-                    console.log(`You win. ${playerSelection} wins against ${computerSelection}`);
-                    playerPoint++;
-                    console.log(playerPoint)
-                }
-                else{
-                    console.log(`You loose. ${playerSelection} looses against ${computerSelection}`);
-                    compPoint++;
+function playerRound() {
+    if (run == true) {
+        let playerSelection = this.innerText;
+        let computerSelection = getComputerChoice();
+        if (playerSelection == computerSelection) {
+            results.innerText = "Draw";
+        }
+        else {
+            for (let i = 0; i < 3; i++) {
+                if (playerSelection == Object.keys(victory)[i]) {
+                    if (computerSelection == victory[Object.keys(victory)[i]]) {
+                        results.innerText = `You win. ${playerSelection} wins against ${computerSelection}`;
+                        playerPoint++;
+                        game(playerPoint, compPoint);
+                    }
+                    else {
+                        results.innerText = `You loose. ${playerSelection} looses against ${computerSelection}`;
+                        compPoint++;
+                        game(playerPoint, compPoint);
+                    }
                 }
             }
         }
     }
 }
-function game(){
-    while(((playerPoint>=5)||(compPoint==5))==false){
-        let computerSelection = getComputerChoice();
-        playerRound(computerSelection);
+function game(playerPoint, compPoint) {
+    if (playerPoint == 5) {
+        winner.innerText = "Congratulations. You won.";
+        run = false;
+        return;
     }
-    if (playerPoint==5){
-        console.log("Congratulations. You won.");
+    else if (compPoint == 5) {
+        winner.innerText = "You loose :(";
+        run = false;
+        return;
     }
-    else{
-        console.log("You loose :(");
+    else {
+        return;
     }
 }
-game();
+
+const buttonRock = document.querySelector('#rock');
+const buttonPaper = document.querySelector('#paper');
+const buttonScissors = document.querySelector('#scissors');
+const results = document.querySelector('#results');
+const winner = document.querySelector('#winner');
+
+buttonRock.addEventListener("click", playerRound);
+buttonPaper.addEventListener("click", playerRound);
+buttonScissors.addEventListener("click", playerRound);
